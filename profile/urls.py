02ae78f7bdf4091241e52500b1d81d4afa6aa8f0
D <asmodzen@gmail.com>
2018-02-18 23:03:59 +0300

    changed label for profile info url

--- profile/urls.py
+++ profile/urls.py
@@ -5,5 +5,5 @@ from profile.views import SignUpView, EditProfileView, ProfileView
 urlpatterns = [
     url(r'^signup/$', SignUpView.as_view(), name='signup'),
     url(r'^profile/edit$', EditProfileView.as_view(), name='edit_profile'),
-    url(r'^profile/$', ProfileView.as_view(), name='profile')
+    url(r'^profile/$', ProfileView.as_view(), name='profile_info')
 ]
