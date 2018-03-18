bcd6ddfbfc98ab82a0497ac03367909e07ea9fca
D <asmodzen@gmail.com>
2018-03-18 19:32:24 +0300

    added search page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from core.views import HomeView
+from core.views import HomeView, SearchView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^search/*$', SearchView.as_view(), name='search'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
+
+from painting.models import Painting
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     login_url = '/login/'
 
     template_name = 'core/home.html'
+
+
+class SearchView(View):
+    template_name = 'core/search.html'
+
+    def get(self, request):
+        qs = Painting.objects.all()
+        context = {
+            'profile': request.user.profile if request.user.is_authenticated else None,
+            'painting_count': Painting.objects.count(),
+            'paintings': qs,
+            'genres': ['жанр1', 'жанр2'],
+            'techniques': ['техника1', 'техника2'],
+        }
+        return render(request, self.template_name, context)
