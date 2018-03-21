e68b9c8ba79259a2d402f273170fd0d00290a323
Denisov <d.denisov@pr-solution.ru>
2018-03-21 22:20:36 +0300

    Revert "added images"
    This reverts commit 1046ff023f8f620d96cb10f4a153ddff8a7dcd09.

--- painting/admin.py
+++ painting/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from .models import Painting
 
 
-class PaintingAdmin(admin.ModelAdmin):
-    save_as = True
-
-
-admin.site.register(Painting, PaintingAdmin)
+admin.site.register(Painting)
